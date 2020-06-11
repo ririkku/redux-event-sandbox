fed391782b6c1655d9ec7e138ec0c4850bb632aa
ririkku <ryoji19950109@gmail.com>
2020-06-11 19:36:21 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onClick={() => console.log("I am clicked")} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
-    return (
+
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Jiro", age: 5},
+    {name: "No name"}
+  ];
+
+  return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>    
+        })
+      }
     </div>
-    )
+  )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+// Componentにpropsが渡されない場合の初期値を`[Compoent名].defaultProps`
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
